4b91cd06e3f336345ee1f39c46a7d1aa7993b78d
Bennett Struttman <ben.struttman@gmail.com>
2022-12-09 14:47:09 -0600

    further golf-ify python day3/p2

--- day3/python/p2.py
+++ day3/python/p2.py
@@ -1,9 +1,5 @@
 f = open('../input.txt','r').read().split('\n')
 toI = lambda c: ord(c) - (96 if ord(c) > 90 else 38)
-lComp = lambda f, i: [c for c in f[i] if c in f[i-1] and c in f[i-2]][0]
-sum = 0
+lComp = lambda i: [c for c in f[i] if c in f[i+1] and c in f[i+2]][0]
 
-for i in range(len(f)):
-	if (i%3==2): sum += toI(lComp(f, i))
-
-print(sum)
+print(sum(map(lambda i: toI(lComp(i)), range(len(f))[::3])))
